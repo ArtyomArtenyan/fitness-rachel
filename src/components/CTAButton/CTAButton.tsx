482ea1522914c8ad8c842731e4a55945dc617f41
ArtyomArtenyan <artyomartenyan.ait@gmail.com>
2026-05-14 20:48:18 +0400

    feat: add CTAButton component

---
+++ src/components/CTAButton/CTAButton.tsx
@@ -0,0 +1,39 @@
+import ArrowButton from '../ArrowButton/ArrowButton';
+
+type CTAButtonProps = {
+	label: string;
+	href?: string;
+	variant?: 'primary' | 'secondary';
+};
+
+const CTAButton = ({
+	label,
+	href = 'https://cal.com/',
+	variant = 'primary',
+}: CTAButtonProps) => {
+	const variants = {
+		primary: {
+			container:
+				'bg-[#f9652f] p-2 text-[#111111] transition-all duration-300 hover:bg-[#121212] hover:text-white',
+			button: 'bg-[#121212] group-hover:bg-[#f9652f]',
+		},
+		secondary: {
+			container:
+				'bg-[#121212] border-[#3c3c3c] border-solid border-1 p-2 text-[#fff] transition-all duration-300 hover:bg-[#212121] hover:text-white',
+			button: 'bg-[#212121] group-hover:bg-[#f9652f]',
+		},
+	};
+
+	const currentVariant = variants[variant];
+
+	return (
+		<div
+			className={`group flex justify-between gap-8 items-center rounded-[10px] md:max-w-fit ${currentVariant.container}`}
+		>
+			<span className='w-fit'>{label}</span>
+			<ArrowButton href={href} bgColor={currentVariant.button} />
+		</div>
+	);
+};
+
+export default CTAButton;
